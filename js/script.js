615d6b554fb9993d93588e181326ecca1e503739
Mia Adjei <adjei.m@gmail.com>
2017-03-02 23:30:40 -0500

    Add make lights function.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,13 @@ function makeRandomColor() {
   // console.log(color);
   return color;
 }
+
+function makeLights() {
+	for ( var i = 0; i < 200; i ++) {
+		var body = document.querySelector('body');
+	  var circleColor = makeRandomColor();
+	  // append a div to the body that has a background color of this color
+
+
+	}
+}
